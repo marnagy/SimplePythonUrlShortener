21dd47dc4f84edfebbf54f531f14f2b598b1dd7c
Marek Nagy <62247071@cuni.cz>
2022-08-11 00:40:21 +0200

    Added support for SQLite via SQLAlchemy using asyncio

--- main.py
+++ main.py
@@ -1,56 +1,22 @@
 from threading import Thread
 from flask import Flask, request, render_template, redirect, Response, jsonify, url_for
 from os import environ
-import string
 import asyncio
 from time import sleep
 from random import Random
-from validators import url as url_validate
+from db import (UrlRecordModel, decode, get_code_for,
+                Session, close_connection, AsyncSession)
+from typing import Callable, Optional
 
+def get_async_delete(url_model: UrlRecordModel, session: AsyncSession, hours: int) -> Callable[[], None]:
+    def res_func():
+        sleep(hours * 60 * 60)
+        session.delete(url_model)
+        asyncio.run(session.commit())
+        UrlRecordModel.USED_NUMBERS.remove(url_model.id)
+        print(f'Model {url_model.id} was deleted.')
 
-BASE_64 = string.ascii_letters + string.digits + '-' + '%'
-base = len(BASE_64)
-rng = Random()
-url_addresses: dict[int, str]  = dict()
-
-def convert_to_BASE64(num: int) -> tuple[bool, str]:
-    if not isinstance(num, int):
-        return (False, None)
-    
-    res_idx: list[int] = list()
-    while num > 0:
-        res_idx.append(num % base)
-        num = num // base
-    
-    return (True, ''.join(
-            map(
-                lambda i: BASE_64[i],
-                res_idx[::-1]
-            )
-        )
-    )
-
-def convert_from_BASE64(code: str) -> tuple[bool, int]:
-    if any(map(lambda x: x not in BASE_64, code)):
-        return (False, -1)
-    
-    indices = list(map(
-        lambda c: BASE_64.index(c),
-        code
-    ))
-    current_power = 0
-    base = len(BASE_64)
-    number = 0
-    for i in indices[::-1]:
-        number += i * pow(base, current_power)
-        current_power += 1
-    
-    return (True, number)
-
-def delete_after(number: int, hours: int) -> None:
-    sleep(hours * 60 * 60)
-    url_addresses.pop(number)
-    #print(f'Deleted number {number}')
+    return res_func
 
 app = Flask('Url shortener')
 
@@ -77,34 +43,38 @@ async def async_test():
 
 ### API ###
 @app.get('/api/shorten')
-def api_shorten():
+async def api_shorten():
     url = request.args.get('url')
-    if not url_validate(url):
+
+    code, url_model = UrlRecordModel.CreateNew(url)
+
+    if code is None or url_model is None:
         return Response('Invalid string: not valid url.', 400)
     
-    number = rng.randint(1, base**30)
-    while number in url_addresses:
-        number = rng.randint(1, base**30)
-    _, shortened = convert_to_BASE64(number)
+    Session.add(url_model)
+    await Session.commit()
     #print(f'Created number {number} for code {shortened}')
-    url_addresses[number] = url
-    thread = Thread(target=lambda: delete_after(number, 1))
+    thread = Thread(target=get_async_delete(url_model, Session, 10))
     thread.start()
-    returned_url = f'{request.host_url}{shortened}'
+    returned_url = f'{request.host_url}{code}'
     return {
         'url': returned_url
     }
 
 @app.get('/<string:code>')
-def redirect_to_url(code:str):
+async def redirect_to_url(code:str):
     if code == 'favicon.ico':
         return Response(status=500)
 
-    valid, number = convert_from_BASE64(code)
+    valid, number = decode(code)
     #print(f'Decoded code {code} as number {number}')
 
-    if valid and number in url_addresses:
-        response_url = url_addresses[number]
+    if valid and number in UrlRecordModel.USED_NUMBERS:
+        url_model: Optional[UrlRecordModel] = await Session.get(UrlRecordModel, number)
+        if url_model is None:
+            return Response(f'Failed to load URL.', 500)
+
+        response_url = url_model.url
         #print(f'Redirecting to {response_url}')
         return Response(status=301, headers={
             'Location': response_url
@@ -112,5 +82,11 @@ def redirect_to_url(code:str):
     else:
         return Response(f'Invalid code provided.', 400)
 
+async def async_main():
+    try:
+        app.run(host='0.0.0.0', port=PORT, debug=DEBUG, threaded=True)
+    finally:
+        await close_connection()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=DEBUG, threaded=True)
+    asyncio.run(async_main())
